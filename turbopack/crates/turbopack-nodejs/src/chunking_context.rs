a72939017ff1d0d64b8e5ae23f26ac9b0b9dc26c
Matt Mastracci <matthew@mastracci.com>
2026-02-19 13:02:27 -0700

    Cleanup pass

--- turbopack/crates/turbopack-nodejs/src/chunking_context.rs
+++ turbopack/crates/turbopack-nodejs/src/chunking_context.rs
@@ -448,7 +448,7 @@ impl ChunkingContext for NodeJsChunkingContext {
 
     #[turbo_tasks::function]
     fn chunking_configs(&self) -> Result<Vc<ChunkingConfigs>> {
-        Ok(Vc::cell(self.chunking_configs.iter().cloned().collect()))
+        Ok(ChunkingConfigs(self.chunking_configs.iter().cloned().collect()).cell())
     }
 
     #[turbo_tasks::function]
