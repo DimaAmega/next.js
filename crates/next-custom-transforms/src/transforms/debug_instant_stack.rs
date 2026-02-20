df31c48d02258c67df1f7cde6698f40b2634299a
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2026-02-20 14:32:00 -0800

    Use `VisitMut`` instead of `Fold` for debug_instant_stack transform
    This matches other transforms and avoids piling on
    existing TODOs moving from Fold to Pass

--- crates/next-custom-transforms/src/transforms/debug_instant_stack.rs
+++ crates/next-custom-transforms/src/transforms/debug_instant_stack.rs
@@ -4,7 +4,7 @@ use swc_core::{
     common::{Span, Spanned},
     ecma::{
         ast::*,
-        visit::{fold_pass, Fold},
+        visit::{visit_mut_pass, VisitMut},
     },
     quote,
 };
@@ -14,7 +14,7 @@ static PAGE_OR_LAYOUT_RE: Lazy<Regex> =
     Lazy::new(|| Regex::new(r"[\\/](page|layout|default)\.(ts|js)x?$").unwrap());
 
 pub fn debug_instant_stack(filepath: String) -> impl Pass {
-    fold_pass(DebugInstantStack {
+    visit_mut_pass(DebugInstantStack {
         filepath,
         instant_export_span: None,
     })
@@ -72,13 +72,13 @@ fn find_var_init_span(items: &[ModuleItem], local_name: &str) -> Option<Span> {
     None
 }
 
-impl Fold for DebugInstantStack {
-    fn fold_module_items(&mut self, items: Vec<ModuleItem>) -> Vec<ModuleItem> {
+impl VisitMut for DebugInstantStack {
+    fn visit_mut_module_items(&mut self, items: &mut Vec<ModuleItem>) {
         if !PAGE_OR_LAYOUT_RE.is_match(&self.filepath) {
-            return items;
+            return;
         }
 
-        for item in &items {
+        for item in items.iter() {
             match item {
                 // `export const unstable_instant = ...`
                 ModuleItem::ModuleDecl(ModuleDecl::ExportDecl(export_decl)) => {
@@ -121,8 +121,6 @@ impl Fold for DebugInstantStack {
         }
 
         if let Some(source_span) = self.instant_export_span {
-            let mut new_items = items;
-
             // TODO: Change React to deserialize errors with a zero-length message
             // instead of using a fallback message ("no message was provided").
             // We're working around this by using a message that is empty
@@ -154,10 +152,7 @@ impl Fold for DebugInstantStack {
                 cons: Expr = cons,
             );
 
-            new_items.push(export);
-            new_items
-        } else {
-            items
+            items.push(export);
         }
     }
 }
